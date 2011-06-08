9c3b38cf0e0fb3a2676055db44e47f28544aeaf5
Mathieu Lecarme <mathieu@garambrogne.net>
2011-06-08 23:51:40 +0200

    explicit name

--- src/metrics_counter.erl
+++ src/metrics_counter.erl
@@ -4,18 +4,22 @@
 -export([
     incr/2,
     get/1,
-    reset/1
+    reset/1,
+    to_list/0
 ]).
 
 %%--------------------------------------------------------------------
 %% Public API
 %%--------------------------------------------------------------------
 
-incr(Key, Inc) ->
-    gen_server:cast(metrics_server, {incr_counter, Key, Inc}).
+incr(Counter, Inc) ->
+    gen_server:cast(metrics_server, {incr_counter, Counter, Inc}).
 
-get(Key) ->
-    gen_server:call(metrics_server, {get_counter, Key}).
+get(Counter) ->
+    gen_server:call(metrics_server, {get_counter, Counter}).
 
-reset(Key) ->
-    gen_server:call(metrics_server, {reset_counter, Key}).
+reset(Counter) ->
+    gen_server:call(metrics_server, {reset_counter, Counter}).
+
+to_list() ->
+    gen_server:call(metrics_server, {list_counter}).
