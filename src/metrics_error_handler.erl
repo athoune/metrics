ecb386e186aef8f6898955f3f40df158a80ee706
Mathieu Lecarme <mathieu@garambrogne.net>
2011-06-20 23:31:21 +0200

    error handler poc

---
+++ src/metrics_error_handler.erl
@@ -0,0 +1,40 @@
+-module(metrics_error_handler).
+-author('mathieu@garambrogne.net').
+
+-behaviour (gen_event).
+
+-export([
+    init/1,
+    terminate/2,
+    code_change/3,
+    handle_call/2,
+    handle_event/2,
+    handle_info/2
+]).
+
+-export([register_with_logger/0]).
+
+-record(state, {}).
+
+register_with_logger() ->
+    error_logger:add_report_handler(?MODULE).
+
+init([]) ->
+    {ok, #state{}}.
+
+handle_event({Type, _Gleader, _Infos}, State) ->
+    metrics_counter:incr(Type),
+    {ok, State}.
+
+handle_call(_Request, State) ->
+    Reply = ok,
+    {ok, Reply, State}.
+
+handle_info(_Info, State) ->
+    {ok, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
